3221cde94e15216e0f0555f52862c3d74ba30d2f
Renato Lopes <renato.ldeoliveira@gmail.com>
2026-05-08 15:47:32 -0300

    update general

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -177,3 +177,11 @@ if (whatsapp) {
     }
   });
 }
+window.addEventListener("scroll", () => {
+  const scrollTop = document.documentElement.scrollTop;
+  const scrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+
+  const progress = (scrollTop / scrollHeight) * 100;
+
+  document.getElementById("progress-bar").style.width = progress + "%";
+});
